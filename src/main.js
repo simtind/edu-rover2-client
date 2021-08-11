1cdeb7e0253007127ac67ce2285dabdc10f635d6
Simen August Tinderholt <simtind@gmail.com>
2021-08-11 23:21:08 +0200

    Update package.json based on tutorial

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const ipc = require('electron').ipcMain;
 const path = require('path');
 const ping = require('ping');
 const WebSocket = require('ws');
 const arp = require('node-arp');
-const os = require("os");
+const os = require("os"); 
+const autoUpdater = require("electron-updater");
 
-function createWindow () {
+function createWindow() {
     const source_dir = path.join(__dirname, "main_window");
     // Create the browser window.
     var window = new BrowserWindow({
@@ -28,24 +29,21 @@ function createWindow () {
     return window;
 }
 
-function get_ips_from_subnet_range(interface_name, cidr)
-{            
+function get_ips_from_subnet_range(interface_name, cidr) {
     const cidr_components = cidr.split("/");
     const ip_string = cidr_components[0];
     const bits = cidr_components[1];
 
-    function ip2int(ipString)
-    {
-        return ipString.split('.').reduce(function(ipInt, octet) { return (ipInt<<8) + parseInt(octet, 10)}, 0) >>> 0;
+    function ip2int(ipString) {
+        return ipString.split('.').reduce(function (ipInt, octet) { return (ipInt << 8) + parseInt(octet, 10) }, 0) >>> 0;
     }
-    function int2ip (ipInt) {
-        return ( (ipInt>>>24) +'.' + (ipInt>>>16 & 255) +'.' + (ipInt>>>8 & 255) +'.' + (ipInt & 255) );
+    function int2ip(ipInt) {
+        return ((ipInt >>> 24) + '.' + (ipInt >>> 16 & 255) + '.' + (ipInt >>> 8 & 255) + '.' + (ipInt & 255));
     }
 
     var ips = [];
 
-    if (bits == 32)
-    {
+    if (bits == 32) {
         // Special case, there are no other devices on the network, and 
         // this value causes the ip_mask evaluation to fail.
         return ips;
@@ -55,41 +53,35 @@ function get_ips_from_subnet_range(interface_name, cidr)
     const host_ip = ip2int(ip_string);
     const subnet_ip = host_ip & ip_mask;
     const num_ips = 2 ** (32 - bits);
-    
-    for (var ip = subnet_ip; ip < subnet_ip + num_ips; ip++)
-    {
-        if (int2ip(ip) == ip_string)
-        {
+
+    for (var ip = subnet_ip; ip < subnet_ip + num_ips; ip++) {
+        if (int2ip(ip) == ip_string) {
             continue;
         }
-        ips.push({interface: interface_name, ip: int2ip(ip)});
+        ips.push({ interface: interface_name, ip: int2ip(ip) });
     }
 
     return ips;
 }
 
-function call_if_alive(host, callback)
-{
+function call_if_alive(host, callback) {
     console.log("Ping " + host["ip"]);
     // Ping IP to see if it contains a live host.
     ping.sys.probe(
-        host["ip"], 
-        function(isAlive)
-        {
+        host["ip"],
+        function (isAlive) {
             console.log(
-                isAlive 
+                isAlive
                     ? `host ${host["ip"]} on network interface ${host["interface"]} is alive`
                     : `host ${host["ip"]} on network interface ${host["interface"]} is not alive`);
-            if (isAlive)
-            {
+            if (isAlive) {
                 arp.getMAC(
-                    host["ip"], 
-                    function(err, mac) 
-                    {
+                    host["ip"],
+                    function (err, mac) {
                         if (!err) {
-                            const is_raspberry =    mac.startsWith("b8:27:eb") |
-                                                    mac.startsWith("dc:a6:32") |
-                                                    mac.startsWith("e4:5f:01");
+                            const is_raspberry = mac.startsWith("b8:27:eb") |
+                                mac.startsWith("dc:a6:32") |
+                                mac.startsWith("e4:5f:01");
                             console.log(`host ${host["ip"]} is ${is_raspberry ? '' : 'not'} an edurov`);
                             callback(mac, is_raspberry);
                         }
@@ -101,19 +93,15 @@ function call_if_alive(host, callback)
     );
 }
 
-function do_ip_search(window)
-{
+function do_ip_search(window) {
     const interfaces = os.networkInterfaces();
     var hosts = []
 
-    for (const key in interfaces)
-    {
+    for (const key in interfaces) {
         const interface = interfaces[key];
-        for (const index in interface)
-        {
+        for (const index in interface) {
             const connection = interface[index];
-            if (connection.internal || connection.family == "IPv6")
-            {
+            if (connection.internal || connection.family == "IPv6") {
                 continue;
             }
 
@@ -123,21 +111,19 @@ function do_ip_search(window)
 
     var prev_percent = 0;
     hosts.forEach(
-        (host, index) => 
-        {
+        (host, index) => {
             const percent = Math.ceil(index * 100 / hosts.length);
-            if (percent > prev_percent)
-            {
+            if (percent > prev_percent) {
                 window.webContents.send("ip_search_progress", percent);
-                prev_percent = percent;   
+                prev_percent = percent;
             }
-            call_if_alive(host, 
-                (mac, is_raspberry) => window.webContents.send("ip_search_result", 
+            call_if_alive(host,
+                (mac, is_raspberry) => window.webContents.send("ip_search_result",
                     {
-                        ip:host["ip"], 
-                        mac:mac,
-                        interface:host["interface"],
-                        is_raspberry:is_raspberry
+                        ip: host["ip"],
+                        mac: mac,
+                        interface: host["interface"],
+                        is_raspberry: is_raspberry
                     }
                 )
             )
@@ -145,15 +131,14 @@ function do_ip_search(window)
     );
 }
 
-function start_main_window(splash_window, address)
-{
+function start_main_window(splash_window, address) {
     const window = createWindow();
     splash_window.close();
 
     ipc.on("get_address", () => window.webContents.send("address", address));
 }
 
-function createSplash () {
+function createSplash() {
     const source_dir = path.join(__dirname, "splash_screen");
 
     // Create the browser window.
@@ -182,20 +167,21 @@ function createSplash () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createSplash()
-  
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createSplash()
-  })
+    autoUpdater.checkForUpdatesAndNotify();
+    createSplash();
+
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createSplash()
+    })
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
